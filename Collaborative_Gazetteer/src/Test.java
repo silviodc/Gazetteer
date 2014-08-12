7c60ba2018061acd6ae467d194ff7ca8703da989
Silvio <silvio.d.cardoso@gmail.com>
2014-08-12 15:21:01 -0300

    building

---
+++ Collaborative_Gazetteer/src/Test.java
@@ -0,0 +1,12 @@
+import read_files.Read_Biodiversity_files;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Read_Biodiversity_files rb = new Read_Biodiversity_files();
+       rb.read_biodiversity_files();
+	}
+
+}
--- Collaborative_Gazetteer/src/read_files/Read_Biodiversity_files.java
+++ Collaborative_Gazetteer/src/read_files/Read_Biodiversity_files.java
@@ -1,5 +1,34 @@
 package read_files;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Read_Biodiversity_files {
 
+	ArrayList<String> paths = new ArrayList<String>();
+	ArrayList<String> data = new ArrayList<String>();
+	
+	public void read_biodiversity_files(){
+		
+		int i=0;
+		
+		 try{     
+	         BufferedReader br = new BufferedReader(new FileReader("/Users/silvio/Documents/Gazetteer/speciesLink.txt"));  
+	  
+	         while(br.ready()){  
+	            String linha = br.readLine();  
+	            String values [] = linha.split("\t");
+	            System.out.println(linha);  
+	           // data.add(linha);
+	            if(i==20)
+	            	return;
+	            i++;
+	         }  
+	         br.close();  
+	      }catch(IOException ioe){  
+	         ioe.printStackTrace();  
+	      }  
+	}
 }
