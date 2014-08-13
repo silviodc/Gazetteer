26a249ca219a227f268ec42ae68693fb82aa4532
silviodc <silvio.d.cardoso@gmail.com>
2014-08-13 02:08:39 -0300

    corrections

--- Collaborative_Gazetteer/src/Test.java
+++ Collaborative_Gazetteer/src/Test.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
 import read_files.Read_Biodiversity_files;
 
 
@@ -6,7 +9,18 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        Read_Biodiversity_files rb = new Read_Biodiversity_files();
-       rb.read_biodiversity_files();
+       try {
+		rb.read_SpeciesLink_biodiversity_files();
+	} catch (UnsupportedEncodingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	}
 
 }
